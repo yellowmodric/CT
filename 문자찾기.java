1686557f8ccee3b09f31e4f19f6f34072bb6bca5
yellowmodric <kyr7091@naver.com>
2025-10-16 02:44:38 +0900

    코테: 문자찾기(강의)

---
+++ 문자찾기.java
@@ -0,0 +1,29 @@
+import java.util.*;
+  
+public class 문자찾기 {
+  public int Solution(String str, char c) {
+    int count = 0;
+    //대소문자를 모두 소문자로 변환
+    str = str.toLowerCase();
+    c = Character.toLowerCase(c);
+    System.out.println(str);
+    
+    //문자열을 모두 돌면서 c가 나오면 count 올려주기
+    for (int i=0; i<str.length(); i++) {
+      if (str.charAt(i) == c) {
+      	count++;
+      }
+    }
+    
+    return count;
+  }
+  
+  public static void main(String[] args){
+    문자찾기 T = new 문자찾기();
+    Scanner sc = new Scanner(System.in);
+    String str = sc.next();
+    char c = sc.next().charAt(0);
+    System.out.println(T.Solution(str, c));
+    sc.close();
+  }
+}
