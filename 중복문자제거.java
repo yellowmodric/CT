7eedad142f1446ea5adede637cd7ef13ac88d0f6
yellowmodric <kyr7091@naver.com>
2025-10-23 19:06:32 +0900

    코테: 중복문자제거(강의)

---
+++ 중복문자제거.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class 중복문자제거 {
+    public String Solution(String str) {
+        String answer = "";
+        
+        //하나씩 순회하며 비교
+        for (int i=0; i<str.length(); i++) {
+            //만약 중복 문자가 아니라면 answer에 추가
+            // System.out.println(str.charAt(i) + " " + i + " " + str.indexOf(str.charAt(i)));
+
+            if (str.indexOf(str.charAt(i)) == i) {
+                answer += str.charAt(i);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        중복문자제거 T = new 중복문자제거();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(T.Solution(str));
+    }
+}
