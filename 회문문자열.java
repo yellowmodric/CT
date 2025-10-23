2c62365eb8506f708693e19c7e61322da89f5892
yellowmodric <kyr7091@naver.com>
2025-10-23 19:25:57 +0900

    다른 방식 풀이: 회문문자열

--- 회문문자열.java
+++ 회문문자열.java
@@ -1,23 +1,45 @@
 import java.util.Scanner;
 
 public class 회문문자열 {
-    public String Solution(String str) {
-        String answer = "YES";
-        String lower = str.toLowerCase();
+    // public String Solution(String str) {
+    //     String answer = "YES";
+    //     String lower = str.toLowerCase();
         
-        int start = 0;
-        int end = str.length() - 1;
+    //     int start = 0;
+    //     int end = str.length() - 1;
 
-        while (start < end) {
-            if (lower.charAt(start) != lower.charAt(end)) {
-                answer = "NO";
-            }
-            start++;
-            end--;
+    //     while (start < end) {
+    //         if (lower.charAt(start) != lower.charAt(end)) {
+    //             return "NO";
+    //         }
+    //         start++;
+    //         end--;
+    //     }
+
+    //     return answer;
+    // }
+
+    // public String Solution(String str) {
+    //     String answer = "YES";
+    //     str = str.toUpperCase();
+    //     int len = str.length();
+    //     for (int i=0; i<len/2; i++) {
+    //         if (str.charAt(i) != str.charAt(len-i-1)) {
+    //             return "NO";
+    //         }
+    //     }
+    //     return answer;
+    // }
+
+    public String Solution(String str) {
+        String answer = "NO";
+        String sb = new StringBuilder(str).reverse().toString();
+        if (str.equalsIgnoreCase(sb)) {
+            return "YES";
         }
-
         return answer;
     }
+
     public static void main(String[] args) {
         회문문자열 T = new 회문문자열();
         Scanner sc = new Scanner(System.in);
