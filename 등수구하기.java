4e4605289bffa76d9ffb637e49563999bf9c98f4
yellowmodric <kyr7091@naver.com>
2025-11-12 18:26:15 +0900

    코테: 등수구하기

---
+++ 등수구하기.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class 등수구하기 {
+    public int[] Solution(int n, int[] arr) {
+        int[] answer = new int[n];
+        
+        for (int i=0; i<n; i++) {
+            answer[i] = 1;
+        }
+
+        for (int i=0; i<n-1; i++) {
+            for (int j=i+1; j<n; j++) {
+                if (arr[i] < arr[j]) {
+                    answer[i]++;
+                } else if (arr[i] > arr[j]) {
+                    answer[j]++;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        등수구하기 T = new 등수구하기();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int x : T.Solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
