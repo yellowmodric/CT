c147c6979fab09430078d378c14b29d7be78f6cd
yellowmodric <kyr7091@naver.com>
2025-11-12 18:41:40 +0900

    다른 방식 풀이: 등수구하기

--- 등수구하기.java
+++ 등수구하기.java
@@ -20,6 +20,20 @@ public class 등수구하기 {
         return answer;
     }
 
+    // 강의 코드
+    // public int[] Solution(int n, int[] arr) {
+    //     int[] answer = new int[n];
+
+    //     for (int i=0; i<n; i++) {
+    //         int cnt = 1;
+    //         for (int j=0; j<n; j++) {
+    //             if (arr[j] > arr[i]) cnt++;
+    //         }
+    //         answer[i] = cnt;
+    //     }
+    //     return answer;
+    // }
+
     public static void main(String[] args) {
         등수구하기 T = new 등수구하기();
         Scanner sc = new Scanner(System.in);
