01aa8ad0c782739c064b4cde456d762661109f9a
yellowmodric <kyr7091@naver.com>
2025-11-12 17:30:19 +0900

    다른 방식 풀이: 점수계산

--- 점수계산.java
+++ 점수계산.java
@@ -23,6 +23,21 @@ public class 점수계산 {
         return answer;
     }
 
+    // 강의코드
+    // public int Solution(int n, int[] arr) {
+    //     int answer = 0, cnt = 0;
+    //     for (int i=0; i<n; i++) {
+    //         if (arr[i] == 1) {
+    //             cnt++;
+    //             answer += cnt;
+    //         } else {
+    //             cnt = 0;
+    //         }
+    //     }
+
+    //     return answer;
+    // }
+
     public static void main(String[] args) {
         점수계산 T = new 점수계산();
         Scanner sc = new Scanner(System.in);
