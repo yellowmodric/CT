a5502672a250269bd1e29f9fa2805c51d7b41398
yellowmodric <kyr7091@naver.com>
2025-12-07 19:30:13 +0900

    다른 방식 풀이: 연속된자연수합

--- 연속된자연수합.java
+++ 연속된자연수합.java
@@ -1,27 +1,56 @@
 import java.util.Scanner;
 
 public class 연속된자연수합 {
+    // public int Solution (int n) {
+    //     int answer = 0;
+    //     int lt = 1;
+    //     int sum = 0;
+
+    //     //시간복잡도 줄이는 방법
+    //     for (int i=1; i<=n/2+1; i++) {
+    //         sum += i;
+
+    //         if (sum == n) {
+    //             answer++;
+    //         } 
+
+    //         while (sum >= n) {
+    //             sum -= lt++;
+
+    //             if (sum == n) {
+    //                 answer++;
+    //             }
+    //         }
+    //     }
+    //     return answer;
+    // }
+
     public int Solution (int n) {
         int answer = 0;
-        int lt = 1;
-        int sum = 0;
+        int sum = 0, lt = 0;
+        int m = n/2 + 1;
+        int[] arr = new int[m];
 
-        //시간복잡도 줄이는 방법
-        for (int i=1; i<=n/2+1; i++) {
-            sum += i;
+        for (int i=0; i<m; i++) {
+            arr[i] = i+1;
+        }
+
+        for (int rt=0; rt<m; rt++) {
+            sum += arr[rt];
 
             if (sum == n) {
                 answer++;
-            } 
+            }
 
             while (sum >= n) {
-                sum -= lt++;
+                sum -= arr[lt++];
 
                 if (sum == n) {
                     answer++;
                 }
             }
         }
+
         return answer;
     }
     
