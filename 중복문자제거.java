08ffe02b34a2176b2adff3fc14b8a3d4e5b9e03c
yellowmodric <kyr7091@naver.com>
2025-12-19 18:53:28 +0900

    설명추가: 중복문자제거

--- 중복문자제거.java
+++ 중복문자제거.java
@@ -8,6 +8,7 @@ public class 중복문자제거 {
         for (int i=0; i<str.length(); i++) {
             //만약 중복 문자가 아니라면 answer에 추가
             // System.out.println(str.charAt(i) + " " + i + " " + str.indexOf(str.charAt(i)));
+            //indexOf 함수는 찾는 원소의 가장 첫번째 인덱스만 반환
 
             if (str.indexOf(str.charAt(i)) == i) {
                 answer += str.charAt(i);
