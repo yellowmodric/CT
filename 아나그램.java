646cc1c3eb2c2dc498d3bcad70aba91fb250efdd
yellowmodric <kyr7091@naver.com>
2025-12-22 02:56:10 +0900

    코드수정: 아나그램

--- 아나그램.java
+++ 아나그램.java
@@ -5,21 +5,25 @@ public class 아나그램 {
     public String Solution(String s1, String s2) {
         String answer = "YES";
         HashMap<Character, Integer> map = new HashMap<>();
-        
-        for (int i=0; i<s1.length(); i++) {
-            map.put(s1.charAt(i), map.getOrDefault(s1.charAt(i), 0) + 1);
+
+        //먼저 첫번째 문자열 map에 넣기
+        for (char c : s1.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
-        for (int i=0; i<s2.length(); i++) {
-            if (!map.containsKey(s2.charAt(i)) || map.get(s2.charAt(i)) == 0) {
+        for (char c : s2.toCharArray()) {
+            //맵에 키가 없을 경우, 개수가 0인 경우는 NO
+            if (!map.containsKey(c) || map.get(c) == 0) {
                 return "NO";
             }
-            map.put(s2.charAt(i), map.get(s2.charAt(i))-1);
+
+            //키가 있을 경우 개수를 계산해야 함
+            //하나씩 빼주기
+            map.put(c, map.get(c) - 1);
         }
-        
         return answer;
     }
-    
+
     public static void main(String[] args) {
         아나그램 T = new 아나그램();
         Scanner sc = new Scanner(System.in);
