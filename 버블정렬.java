d87d4a0c6f76d7c9dcd995768a767d4dd3f979e5
yellowmodric <kyr7091@naver.com>
2026-01-04 19:13:04 +0900

    코테: 버블정렬

--- 버블정렬.java
+++ 버블정렬.java
@@ -1,30 +1,24 @@
 import java.util.Scanner;
 
-public class 선택정렬 {
+public class 버블정렬 {
     public int[] Solution(int n, int[] arr) {
-        //선택정렬은 차례대로 비교해 가장 작은 값을 찾아 첫번째에 놓고~ 방식
-        //1회전 때 min을 가장 앞자리로
-        //항상 min을 찾아야 함
+        //버블정렬은 가장 큰 숫자부터 뒤에다 확정시키는 것
+        //제일 큰 숫자가 뒤로 계속 미뤄져서 뒤부터 확정
 
         for (int i=0; i<n-1; i++) {
-            int idx = i;
-
-            for (int j=i+1; j<n; j++) {
-                if (arr[idx] > arr[j]) {
-                    idx = j;
+            for (int j=0; j<n-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
                 }
             }
-
-            int tmp = arr[idx];
-            arr[idx] = arr[i];
-            arr[i] = tmp;
         }
-
         return arr;
     }
-
+    
     public static void main(String[] args) {
-        선택정렬 T = new 선택정렬();
+        버블정렬 T = new 버블정렬();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
