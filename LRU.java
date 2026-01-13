6412975a39b6b7e56f8cd6e251eee60232e7616c
yellowmodric <kyr7091@naver.com>
2026-01-13 21:28:23 +0900

    코테: LRU

--- LRU.java
+++ LRU.java
@@ -5,28 +5,28 @@ public class LRU {
         int[] answer = new int[s];
 
         for (int x : arr) {
-            int pos = -1;
+            int tmp = -1;
 
             for (int i=0; i<s; i++) {
-                if (x == answer[i]) {
-                    pos = i;
+                if (answer[i] == x) {
+                    tmp = i;
                 }
             }
 
-            if (pos == -1) {
-                for (int i=s-1; i>=1; i--) {
-                    answer[i] = answer[i-1];
+            if (tmp == -1) {
+                for (int i=s-2; i>=0; i--) {
+                    answer[i+1] = answer[i];
                 }
             } else {
-                for (int i=pos; i>=1; i--) {
-                    answer[i] = answer[i-1];
+                for (int i=tmp-1; i>=0; i--) {
+                    answer[i+1] = answer[i];
                 }
             }
             answer[0] = x;
         }
         return answer;
     }
-    
+
     public static void main(String[] args) {
         LRU T = new LRU();
         Scanner sc = new Scanner(System.in);
