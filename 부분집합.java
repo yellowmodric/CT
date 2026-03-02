9bee587d8e8cb3b9d01ee8142fb309a3cf7823d2
yellowmodric <kyr7091@naver.com>
2026-03-02 16:24:42 +0900

    코테: 부분집합

---
+++ 부분집합.java
@@ -0,0 +1,24 @@
+public class 부분집합 {
+    static int n;
+    static int[] ch;
+    public void DFS(int L) {
+        if (L == n+1) {
+            String tmp = "";
+            for (int i=1; i<=n; i++) {
+                if (ch[i] == 1) tmp += (i+ " ");
+            }
+            if (tmp.length() > 0) System.out.println(tmp);
+        } else {
+            ch[L] = 1;
+            DFS(L+1);
+            ch[L] = 0;
+            DFS(L+1);
+        }
+    }
+    public static void main(String[] args) {
+        부분집합 T = new 부분집합();
+        n = 3;
+        ch = new int[n+1];
+        T.DFS(1);
+    }
+}
